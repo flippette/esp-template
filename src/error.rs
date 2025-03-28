8cd1abe9c7f7b31070f2774b121764c80e1d957d
flippette <me@flippo.cc>
2025-03-28 05:32:44 +0200

    switch Error::AdHoc to &'static str, add FallibleExt

--- src/error.rs
+++ src/error.rs
@@ -4,6 +4,23 @@ use defmt::Str;
 use embassy_executor::SpawnError;
 
 crate::error_def! {
-    AdHoc => Str = "ad-hoc error: {}",
+    AdHoc => &'static str = "ad-hoc error: {}",
     Spawn => SpawnError = "task spawn error: {}",
+}
+
+/// Utilities for [`Option`] and [`Result`].
+pub trait FallibleExt<T>: Sized {
+    fn or_adhoc(self, msg: &'static str) -> Result<T, Error>;
+}
+
+impl<T> FallibleExt<T> for Option<T> {
+    fn or_adhoc(self, msg: &'static str) -> Result<T, Error> {
+        self.ok_or(Error::AdHoc(msg))
+    }
+}
+
+impl<T, E> FallibleExt<T> for Result<T, E> {
+    fn or_adhoc(self, msg: &'static str) -> Result<T, Error> {
+        self.or(Err(Error::AdHoc(msg)))
+    }
 }
