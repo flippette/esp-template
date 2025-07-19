8089c539ebd040aebca3dfc004f8022253acd9dd
flippette <me@flippo.cc>
2025-07-19 23:19:34 +0700

    switch back to defmt::Str for AdHoc

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,13 @@
 //! error handling.
 
+use defmt::Str;
 use embassy_executor::SpawnError;
 
 crate::error! {
   /// common error type.
   #[derive(Clone)]
   pub enum Error {
-    AdHoc(&'static str) => "ad-hoc error: {}",
+    AdHoc(Str) => "ad-hoc error: {}",
     Spawn(SpawnError) => "task spawn error: {}",
   }
 }
@@ -14,17 +15,17 @@ crate::error! {
 /// utils for [`Option`] and [`Result`].
 #[allow(dead_code)]
 pub trait FallibleExt<T>: Sized {
-  fn or_adhoc(self, msg: &'static str) -> Result<T, Error>;
+  fn or_adhoc(self, msg: Str) -> Result<T, Error>;
 }
 
 impl<T> FallibleExt<T> for Option<T> {
-  fn or_adhoc(self, msg: &'static str) -> Result<T, Error> {
+  fn or_adhoc(self, msg: Str) -> Result<T, Error> {
     self.ok_or(Error::AdHoc(msg))
   }
 }
 
 impl<T, E> FallibleExt<T> for Result<T, E> {
-  fn or_adhoc(self, msg: &'static str) -> Result<T, Error> {
+  fn or_adhoc(self, msg: Str) -> Result<T, Error> {
     self.or(Err(Error::AdHoc(msg)))
   }
 }
