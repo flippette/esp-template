d23436c604f92e33a088d5e43653399a3f8d6a20
flippette <me@flippo.cc>
2025-09-25 21:48:11 +0300

    replace FallibleExt with ToAdHocError and Force

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,6 @@
 //! error handling.
 
-use defmt::Str;
+use defmt::{Format, Str, unwrap};
 
 crate::macros::error! {
   /// common error type.
@@ -9,24 +9,57 @@ crate::macros::error! {
   }
 }
 
+/// make a [`Result`] with an [`Error::AdHoc`].
+pub trait ToAdHocError<T> {
+  fn or_adhoc(self, msg: Str) -> Result<T, Error>
+  where
+    Self: Sized;
+}
 
+/// trait-level `unwrap()`, using [`defmt`].
+pub trait Force<T>: Sized {
+  fn force(self) -> T;
+}
+
+impl<T> ToAdHocError<T> for Option<T> {
+  fn or_adhoc(self, msg: Str) -> Result<T, Error>
+  where
+    Self: Sized,
+  {
+    self.ok_or(msg.into())
   }
 }
 
-/// utils for [`Option`] and [`Result`].
-#[allow(dead_code)]
-pub trait FallibleExt<T>: Sized {
-  fn or_adhoc(self, msg: Str) -> Result<T, Error>;
-}
-
-impl<T> FallibleExt<T> for Option<T> {
-  fn or_adhoc(self, msg: Str) -> Result<T, Error> {
-    self.ok_or(Error::AdHoc(msg))
+impl<T, E> ToAdHocError<T> for Result<T, E> {
+  fn or_adhoc(self, msg: Str) -> Result<T, Error>
+  where
+    Self: Sized,
+  {
+    self.map_err(|_| msg.into())
   }
 }
 
-impl<T, E> FallibleExt<T> for Result<T, E> {
-  fn or_adhoc(self, msg: Str) -> Result<T, Error> {
-    self.or(Err(Error::AdHoc(msg)))
+impl<T> Force<T> for Option<T> {
+  fn force(self) -> T {
+    unwrap!(self)
   }
+}
+
+impl<T, E> Force<T> for Result<T, E>
+where
+  E: Format,
+{
+  fn force(self) -> T {
+    unwrap!(self)
+  }
+}
+
+// `() -> ()` trivially
+impl Force<()> for () {
+  fn force(self) {}
+}
+
+// `! -> ()` trivially
+impl Force<()> for ! {
+  fn force(self) {}
 }
